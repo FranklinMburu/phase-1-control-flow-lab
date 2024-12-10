89be1eca4c24170a2f9a9f22c6f93ec1b6c248a5
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2024-12-10 18:27:27 +0300

    3 types of control flow

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Function to determine the greeting based on distance
+function scuberGreetingForFeet(feet) {
+  // Check if the ride distance is less than or equal to 400 feet
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  // Check if the ride distance is between 400 and 2000 feet
+  else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  // Check if the ride distance is over 2000 feet but not more than 2500 feet
+  else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  // If the ride distance exceeds 2500 feet, it's not allowed
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check the city using a ternary operator
+function ternaryCheckCity(city) {
+  // Return response based on whether the city is NYC or not
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to determine the response based on the tip using a switch statement
+function switchOnCharmFromTip(tip) {
+  // Use a switch statement to match the tip level
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
